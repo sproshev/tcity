f0a1e33424f6a1974d7be86b5be934bf4885fdb0
Semyon Proshev <s-proshev@ya.ru>
2014-11-29 16:04:10 +0300

    schema helper

---
+++ app/src/main/kotlin/com/tcity/android/db/SchemaHelper.kt
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2014 Semyon Proshev
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.tcity.android.db
+
+import android.database.sqlite.SQLiteOpenHelper
+import android.database.sqlite.SQLiteDatabase
+import android.content.Context
+
+public abstract class AbstractSchemaHelper(
+        private val schema: AbstractSchema,
+        context: Context,
+        name: String,
+        version: Int
+) : SQLiteOpenHelper(context, name, null, version) {
+
+    override fun onCreate(db: SQLiteDatabase) {
+        create(schema, db)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        upgrade(schema, db, oldVersion, newVersion)
+    }
+}
+
+public class BuildSchemaHelper(context: Context) : AbstractSchemaHelper(BuildSchema(), context, "builds", 1)
+
+public class BuildConfigurationSchemaHelper(context: Context) : AbstractSchemaHelper(BuildSchema(), context, "buildconfigurations", 1)
+
+public class ProjectSchemaHelper(context: Context) : AbstractSchemaHelper(BuildSchema(), context, "projects", 1)
+
+
+
